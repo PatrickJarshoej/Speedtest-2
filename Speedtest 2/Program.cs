e40ec57260ddc67b7e2db5e573a49f8f9e4d2b18
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-04-08 09:59:05 +0200

    Done

--- Speedtest 2/Program.cs
+++ Speedtest 2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            List<string> Ingredients = new();
+
+            Ingredients.Add("200g hvedemel, (ca 3 1/2 dl)");
+            Ingredients.Add("2 teskefulde bagepulver");
+            Ingredients.Add("1/2 teskefuld fint salt");
+            Ingredients.Add("2 æg");
+            Ingredients.Add("150 g sukker (ca 1 3/4 dl)");
+            Ingredients.Add("1 teskefuld vaniljesukker");
+            Ingredients.Add("125 g smeltet smør");
+            Ingredients.Add("2 modene bananer");
+            Ingredients.Add("100 g mørk chokolade");
+
+            foreach (string ingredient in Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
         }
     }
 }
