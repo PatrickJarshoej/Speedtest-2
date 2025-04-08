10bcf6d0f383decfec8137c65ea998ddb655d4fe
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-04-08 09:04:38 +0200

    Add project files.

---
+++ Speedtest 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Speedtest_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
